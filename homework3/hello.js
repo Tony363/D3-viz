de6a9b65bf9570ffdeee4cf82ac110c88cfed00e
tony363 <pysolver33@gmail.com>
2020-05-07 21:23:52 +0800

    fucking git

---
+++ homework3/hello.js
@@ -0,0 +1,7 @@
+var x = 1;
+if (window.location.search == '?ap=true'){
+    window.document.getElementById('title').innerHTML = "US Area Census"
+}else{
+    window.document.getElementById('title').innerHTML = "US Population Census"
+
+}
